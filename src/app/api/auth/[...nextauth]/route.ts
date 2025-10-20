07bd93ba58e8652576b66cb9987dc84395827e0d
minh2002811 <minh2002811@gmail.com>
2025-10-20 16:24:56 +0700

    sửa lỗi build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import GitHubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import LinkedInProvider from "next-auth/providers/linkedin";
 
-export const authOptions = {
+ const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
